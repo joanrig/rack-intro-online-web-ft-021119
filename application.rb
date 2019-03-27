f043a0de377a20cc42a86afce2ca2cb8e2045c3b
Joan Lyness <joanrigdon@gmail.com>
2019-03-27 00:26:23 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World, my name is Joan."
+    resp.write "Hello, my name is Joan."
     resp.finish
   end
 
